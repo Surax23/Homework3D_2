77bed89cba129d52307545ba4c70f6ec219c31d5
Александр Сурков <rm2kfiles@ya.ru>
2019-02-10 14:05:35 +0300

    hw4

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,18 +5,21 @@ namespace Geekbrains
 {
 	public class Main : MonoBehaviour
 	{
-        public static System.Random random = new System.Random();
-
 		public FlashLightController FlashLightController { get; private set; }
 		public InputController InputController { get; private set; }
 		public PlayerController PlayerController { get; private set; }
 		public WeaponController WeaponController { get; private set; }
+		public SelectionController SelectionController { get; private set; }
+		public BotController BotController { get; private set; }
 		public ObjectManager ObjectManager { get; private set; }
-		public Transform Player { get; private set; }
-		public Transform MainCamera { get; private set; }
 		private BaseController[] Controllers;
 
+		public Transform Player { get; private set; }
+		public Transform MainCamera { get; private set; }
+
 		public static Main Instance { get; private set; }
+		public Bot RefBotPrefab;
+		public int CountBot;
 
 		private void Awake()
 		{
@@ -26,20 +29,32 @@ namespace Geekbrains
 			Player = GameObject.FindGameObjectWithTag("Player").transform;
 
 			ObjectManager = new ObjectManager();
-			ObjectManager.Start();
 			
-			PlayerController = new PlayerController(new UnitMotor(
-				GameObject.FindObjectOfType<CharacterController>().transform));
-			PlayerController.On();
+			PlayerController = new PlayerController(new UnitMotor(Player));
+			
 			FlashLightController = new FlashLightController();
 			InputController = new InputController();
-			InputController.On();
+			SelectionController = new SelectionController();
 			WeaponController = new WeaponController();
-			Controllers = new BaseController[4];
+
+			BotController = new BotController();
+
+			Controllers = new BaseController[6];
 			Controllers[0] = FlashLightController;
 			Controllers[1] = InputController;
 			Controllers[2] = PlayerController;
 			Controllers[3] = WeaponController;
+			Controllers[4] = SelectionController;
+			Controllers[5] = BotController;
+		}
+
+		private void Start()
+		{
+			ObjectManager.Start();
+			InputController.On();
+			PlayerController.On();
+			BotController.On();
+			BotController.Init(CountBot);
 		}
 
 		private void Update()
